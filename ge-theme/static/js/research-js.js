b2d6a76a33ae701bd959809588c60929a31e5c2a
William Luke <william@atto-byte.com>
2023-03-10 08:28:31 +0300

    fix: alert on research page load

--- ge-theme/static/js/research-js.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    debugger;
-    $.get('/scrapped-text/reseasrch/article-head.txt', function (data) {
-        var lines = data.split("\n")
-        alert("file-read ok")
-    }, 'text');
-});
