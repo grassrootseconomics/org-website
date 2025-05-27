388aa805989c60e0756180d12ddcf3f3b69982af
Mohammed Sohail <sohailsameja@gmail.com>
2025-05-27 17:03:21 +0300

    add blog link

--- pelicanconf.py
+++ pelicanconf.py
@@ -47,7 +47,7 @@ MENUITEMS = (
     ('Eco', '/pages/food-forests.html'),
     ('Edu', '/pages/research.html'),
     ('Docs', 'https://docs.grassecon.org'),
-    ('Blog', '/category/blog.html'),
+    ('Blog', 'https://grassecon.substack.com'),
     ('Support', '/pages/get-involved.html'),
     ('Contact', '/pages/contact-us.html')
 )
